a2026ea1d35b4ab371fe1bbb6e000aa2e66e9d1f
Pranit Bauva <pranit.bauva@gmail.com>
2015-12-20 16:14:44 +0530

    Use some error handling in case malloc does not return a pointer and use x0 instead of x[0]

--- simple_euler.c
+++ simple_euler.c
@@ -31,6 +31,9 @@ int main(int argc, char **argv){
 double simple_euler(double x0, double y0, double xf, long steps){
     double *x = (double *) malloc((size_t) (steps * sizeof(double)));
     double *y = (double *) malloc(steps * sizeof(double));
+    if(!x)
+        if(!y)
+            fprintf(stderr, "Sorry there was some problem allocating memory for the program.");
     x[0] = x0;
     x[steps-1] = xf;
     y[0] = y0;
@@ -38,7 +41,7 @@ double simple_euler(double x0, double y0, double xf, long steps){
     double h = (xf-x0)/steps;
 
     for(i = 0; i < steps; i++)
-        x[i] = i*h + x[0];
+        x[i] = i*h + x0;
 
     for(i = 1; i < steps; i++)
         y[i] = y[i-1] + h * f_dash(x[i-1]);
