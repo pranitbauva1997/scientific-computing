b91de3128089a343b98bb998908cf9cb6ed6801c
Pranit Bauva <pranit.bauva@gmail.com>
2015-12-31 15:32:52 +0530

    Fix bug in modified_midpoint.c
    Did not include the method in ode.h so it might have reference to some arbitary
    location thus giving inaccurate results.

--- modified_midpoint.c
+++ modified_midpoint.c
@@ -18,10 +18,10 @@ double modified_midpoint(double x0, double y0, double xf, long steps){
     y[1] = y0 + h*f_dash(x0, y0);
 
     for(i = 1; i < steps - 1; i++){
-        y[i+1] = y[0] + 2*h*(f_dash(x[i], y[i]));
+        y[i+1] = y[i-1] + 2*h*(f_dash(x0 + h*i, y[i]));
     }
 
-    double yf = y[steps - 1];
+    double yf = (y[steps - 1] + y[steps - 2] + h*f_dash(xf, y[steps - 1]))/2;
     free(x);
     free(y);
     return yf;
--- ode.h
+++ ode.h
@@ -2,3 +2,4 @@ double f_dash(double x, double y);
 double simple_euler(double x0, double y0, double xf, long steps);
 double midpoint_euler(double x0, double y0, double xf, long steps);
 double four_order_simple_rk(double x0, double y0, double xf, long steps);
+double modified_midpoint(double x0, double y0, double xf, long steps);
